65a7c24972aaa767bfa9f008cce47b9c885f2661
RehemaDShemangale <61310320+RehemaDShemangale@users.noreply.github.com>
2020-05-13 20:35:11 +0300

    Add files via upload

--- Courses.js
+++ Courses.js
@@ -3,13 +3,103 @@ import React, { Component} from 'react';
 import { View, Image} from 'react-native';
 import { Text } from 'galio-framework'
 import {Entypo,AntDesign} from '@expo/vector-icons';
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TouchableOpacity, TouchableNativeFeedback, TouchableHighlight } from 'react-native-gesture-handler';
 
 
 export default class courses extends Component{
 
   static navigationOptions={
     headerShown: false
+}
+
+focus=()=>{
+  this.setState({log:"unlog"})
+
+}
+
+constructor(props) {
+  super(props);
+  this.state = {
+    log: "unlog",
+  }
+}
+
+Enrolled =()=>{
+  if (this.state.log=="uneroll"){
+    return(
+
+    <View>
+    <View style={{backgroundColor:'#f1f2f6',borderRadius:5,marginBottom:15,height:255,alignItems:'center'}}>
+      <Image source={require('./assets/swahil.jpg')} style={{height:250,width:400}} />
+    </View>
+
+    <Text style={{color:'rgba(88, 189, 255,5)',marginTop:15,paddingHorizontal:5,fontSize:17,marginBottom:10}}>Course Name:  KISWAHILI KWA WAANDISHI WA HABARI</Text>
+    <Text style={{marginTop:15,marginBottom:10,paddingHorizontal:5,fontSize:14}}>Ni kwa ajili yakuwafanya waandishi wa Habari waweze kutumia Kiswahili Fasihi. </Text>
+    <Text style={{marginTop:10,paddingHorizontal:5,fontSize:16,marginBottom:20,marginTop:20}}>Lessons</Text>
+
+    <TouchableOpacity onPress={()=>this.props.navigation.navigate ('Lessons')}>
+    <View style={{borderColor:'rgba(88, 189, 255,10)',borderWidth:2,marginTop:20, borderRadius:5,paddingHorizontal:10,paddingTop:10,paddingBottom:15}}>
+          <Text style={{color:'rgba(88, 189, 255,5)',fontSize:14,marginTop:10,marginBottom:10}}>Post on 2020-04-06 07:26:34</Text>
+          <Text style={{marginTop:15,fontSize:14,fontWeight:'bold',marginBottom:10}}>1. Aina Za Maneno</Text>
+          <Text style={{marginTop:15,fontSize:14}}>kufahamu maana ya</Text>
+          <Text>neno, nomino, kiwakilishi, kivumishi, kitenzi, kielezi, kiunganishi, kihisishi. </Text>
+    </View>
+    </TouchableOpacity>
+    <View style={{height:50}} />
+    </View>
+
+    );
+
+  }
+  else if(this.state.log=="uneroll") {
+
+    return(
+
+      <View>
+      <View style={{backgroundColor:'#f1f2f6',borderRadius:5,marginBottom:15,height:255,alignItems:'center'}}>
+        <Image source={require('./assets/swahil.jpg')} style={{height:250,width:400}} />
+      </View>
+  
+      <Text style={{color:'rgba(88, 189, 255,5)',marginTop:15,paddingHorizontal:5,fontSize:17,marginBottom:10}}>Course Name:  KISWAHILI KWA WAANDISHI WA HABARI</Text>
+      <Text style={{marginTop:15,marginBottom:10,paddingHorizontal:5,fontSize:14}}>Ni kwa ajili yakuwafanya waandishi wa Habari waweze kutumia Kiswahili Fasihi. </Text>
+      <Text style={{marginTop:10,paddingHorizontal:5,fontSize:16,marginBottom:20,marginTop:20}}>Lessons</Text>
+  
+      <View style={{borderColor:'rgba(88, 189, 255,10)',borderWidth:2,marginTop:20, borderRadius:5,paddingHorizontal:10,paddingTop:10,paddingBottom:15}}>
+            <Text style={{color:'rgba(88, 189, 255,5)',fontSize:14,marginTop:10,marginBottom:10}}>Post on 2020-04-06 07:26:34</Text>
+            <Text style={{marginTop:15,fontSize:14,fontWeight:'bold',marginBottom:10}}>1. Aina Za Maneno</Text>
+            <Text style={{marginTop:15,fontSize:14}}>kufahamu maana ya</Text>
+            <Text>neno, nomino, kiwakilishi, kivumishi, kitenzi, kielezi, kiunganishi, kihisishi. </Text>
+      </View>
+      <TouchableOpacity>
+      <Text style={{backgroundColor:'#1e90ff',color:'white',textAlign:'center',textAlignVertical:'center',marginTop:10,height:40,borderRadius:10}}>ENROLL</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+      <Text style={{backgroundColor:'#1e90ff',color:'white',textAlign:'center',textAlignVertical:'center',marginTop:10,height:40,borderRadius:10}}>ENROLL</Text>
+      </TouchableOpacity>
+      <View style={{height:50}} />
+      </View>
+  
+    );
+
+  }
+
+  else
+      return(
+
+          <View>
+      
+          <Text style={{color:'rgba(88, 189, 255,5)',marginTop:15,paddingHorizontal:5,fontSize:17,marginBottom:10}}>Course Name:  KISWAHILI KWA WAANDISHI WA HABARI</Text>
+          <Text style={{marginTop:10,paddingHorizontal:5,fontSize:16,marginBottom:20,marginTop:20}}>Lessons</Text>
+      
+          <TouchableOpacity>
+          <Text style={{backgroundColor:'#1e90ff',color:'white',textAlign:'center',textAlignVertical:'center',marginTop:10,height:40,borderRadius:10}}>LOGIN TO ENROLL</Text>
+          </TouchableOpacity>
+          </View>
+      
+        );
+
+      
+
 }
 
     render(){
@@ -35,23 +125,7 @@ export default class courses extends Component{
           style={{marginHorizontal:10,paddingTop:10,}}
       >
 
-          <View style={{backgroundColor:'#f1f2f6',borderRadius:5,marginBottom:15,height:255,alignItems:'center'}}>
-            <Image source={require('./assets/swahil.jpg')} style={{height:250,width:400}} />
-          </View>
-
-          <Text style={{color:'rgba(88, 189, 255,5)',marginTop:15,paddingHorizontal:5,fontSize:17,marginBottom:10}}>Course Name:  KISWAHILI KWA WAANDISHI WA HABARI</Text>
-          <Text style={{marginTop:15,marginBottom:10,paddingHorizontal:5,fontSize:14}}>Ni kwa ajili yakuwafanya waandishi wa Habari waweze kutumia Kiswahili Fasihi. </Text>
-          <Text style={{marginTop:10,paddingHorizontal:5,fontSize:16,marginBottom:20,marginTop:20}}>Lessons</Text>
-
-          <TouchableOpacity onPress={()=>this.props.navigation.navigate ('Lessons')}>
-          <View style={{borderColor:'rgba(88, 189, 255,10)',borderWidth:2,marginTop:20, borderRadius:5,paddingHorizontal:10,paddingTop:10,paddingBottom:15}}>
-                <Text style={{color:'rgba(88, 189, 255,5)',fontSize:14,marginTop:10,marginBottom:10}}>Post on 2020-04-06 07:26:34</Text>
-                <Text style={{marginTop:15,fontSize:14,fontWeight:'bold',marginBottom:10}}>1. Aina Za Maneno</Text>
-                <Text style={{marginTop:15,fontSize:14}}>kufahamu maana ya</Text>
-                <Text>neno, nomino, kiwakilishi, kivumishi, kitenzi, kielezi, kiunganishi, kihisishi. </Text>
-          </View>
-          </TouchableOpacity>
-          <View style={{height:50}} />
+         {this.Enrolled()}
       
       </ScrollView>
         
